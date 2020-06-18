63bca848e8663fb7745c938cae3a5d388e5a4f2f
davjam <45334176+davjam@users.noreply.github.com>
2020-06-18 14:07:29 +0100

    Some renaming
    Iterator -> Injector
    ItPlan -< MapPlan
    Consistent type/variable names

--- src/MapWith.hs
+++ src/MapWith.hs
@@ -4,7 +4,7 @@
 
 module MapWith
   (
-    Iterator(..)
+    Injector(..)
   , limIt, ixIt, zipIt
   , adjElt
   
@@ -19,58 +19,69 @@ where
 import Data.Foldable (toList)
 import Data.Traversable (mapAccumL, mapAccumR)
 
-data Iterator a i = forall s. Iterator
-  (i -> s -> (a, s)) --like a transformer, but also takes a value from the set we're mapping over
+{-
+Common type/value names:
+a = the value in the Traversable we're mapping over
+b = the result in the final Traversable
+
+i = an output from an Injector, injected into the mapper
+l = an i that's determined from the left
+r = an i that's determined from the right
+
+s = internal state in an Injector
+-}
+
+data Injector a i = forall s. Injector
+  (a -> s -> (i, s)) --like a transformer, but also takes a value from the set we're mapping over
   s                  --initial state
 
-pairIt :: Iterator a i -> Iterator b i -> Iterator (a, b) i
-pairIt (Iterator n1 z1) (Iterator n2 z2) = Iterator nxt (z1, z2)
+pairIt :: Injector a i1 -> Injector a i2 -> Injector a (i1, i2)
+pairIt (Injector n1 z1) (Injector n2 z2) = Injector nxt (z1, z2)
   where
-  nxt i (s1, s2) = let (x1, s1') = n1 i s1
-                       (x2, s2') = n2 i s2
-                   in ((x1, x2), (s1', s2'))
+  nxt a (s1, s2) = let (i1, s1') = n1 a s1
+                       (i2, s2') = n2 a s2
+                   in ((i1, i2), (s1', s2'))
 
-nullIt :: Iterator () i
-nullIt = Iterator (\_ _ -> ((), ())) ()
+nullIt :: Injector a ()
+nullIt = Injector (\_ _ -> ((), ())) ()
 
-limIt :: Iterator Bool i
-limIt = Iterator (\_ x -> (x, False)) True
+limIt :: Injector a Bool
+limIt = Injector (\_ i -> (i, False)) True
 
-ixIt :: Integral a => Iterator a i
-ixIt = Iterator (\_ n -> (n, n+1)) 0
+ixIt :: Integral i => Injector a i
+ixIt = Injector (\_ i -> (i, i+1)) 0
 
-zipIt :: Foldable f => f a -> Iterator a i --unsafe if we run off the end
-zipIt f = Iterator (\_ x -> (head x, tail x)) (toList f)
+zipIt :: Foldable f => f i -> Injector a i --unsafe if we run off the end
+zipIt f = Injector (\_ x -> (head x, tail x)) (toList f)
 
-adjElt :: Iterator (Maybe i) i
-adjElt = Iterator (\x prevMay -> (prevMay, Just x)) Nothing
+adjElt :: Injector a (Maybe a)
+adjElt = Injector (\a prevMay -> (prevMay, Just a)) Nothing
 
-itMapL :: Traversable t => forall b a. Iterator b i -> t (i, (b -> a)) -> t (i, a)
-itMapL (Iterator nxt sInit) = snd . mapAccumL applyBound sInit
-  where
-  applyBound s (i, f) = let (x, s') = nxt i s in (s', (i, f x))
+itMapL :: Traversable t => Injector a i -> t (a, i -> b) -> t (a, b)
+itMapL (Injector n z) = snd . mapAccumL (acc n) z
 
-itMapR :: Traversable t => forall b a. Iterator b i -> t (i, (b -> a)) -> t (i, a)
-itMapR (Iterator nxt sInit) = snd . mapAccumR applyBound sInit
-  where
-  applyBound s (i, f) = let (x, s') = nxt i s in (s', (i, f x))
+itMapR :: Traversable t => Injector a i -> t (a, i -> b) -> t (a, b)
+itMapR (Injector n z) = snd . mapAccumR (acc n) z
 
-data ItPlan a l r b = ItPlan (a -> l -> r -> b) (Iterator l a) (Iterator r a)
+acc :: (a -> s -> (i, s)) -> s -> (a, i -> b) -> (s, (a, b))
+acc n s (a, f) = let (i, s') = n a s in (s', (a, f i))
 
-($->) :: (a -> l -> b) -> Iterator l a -> ItPlan a l () b
-f $-> it = ItPlan (\a l _ -> f a l) it nullIt
+data MapPlan a l r b = MapPlan (a -> l -> r -> b) (Injector a l) (Injector a r)
 
-(<-$) :: (a -> r -> b) -> Iterator r a -> ItPlan a () r b
-f <-$ it = ItPlan (\a _ r -> f a r) nullIt it
+($->) :: (a -> l -> b) -> Injector a l -> MapPlan a l () b
+f $-> it = MapPlan (\a l _ -> f a l) it nullIt
 
-(*->) :: ItPlan a l r (l' -> b) -> Iterator l' a -> ItPlan a (l, l') r b
-ItPlan f itL itR *-> itL' = ItPlan (\a (l, l') r -> f a l r l') (pairIt itL itL') itR
+(<-$) :: (a -> r -> b) -> Injector a r -> MapPlan a () r b
+f <-$ it = MapPlan (\a _ r -> f a r) nullIt it
 
-(<-*) :: ItPlan a l r (r' -> b) -> Iterator r' a -> ItPlan a l (r, r') b
-ItPlan f itL itR <-* itR' = ItPlan (\a l (r, r') -> f a l r r') itL (pairIt itR itR')
+(*->) :: MapPlan a l r (l' -> b) -> Injector a l' -> MapPlan a (l, l') r b
+MapPlan f itL itR *-> itL' = MapPlan (\a (l, l') r -> f a l r l') (pairIt itL itL') itR
 
-mapWith :: Traversable t => ItPlan a l r b -> t a -> t b
-mapWith (ItPlan f itL itR) l = fmap snd $ itMapR itR $ itMapL itL $ fmap (\x -> (x, f x)) l
+(<-*) :: MapPlan a l r (r' -> b) -> Injector a r' -> MapPlan a l (r, r') b
+MapPlan f itL itR <-* itR' = MapPlan (\a l (r, r') -> f a l r r') itL (pairIt itR itR')
+
+mapWith :: Traversable t => MapPlan a l r b -> t a -> t b
+mapWith (MapPlan f itL itR) l = fmap snd $ itMapR itR $ itMapL itL $ fmap (\a -> (a, f a)) l
 
 withFirstLast :: Traversable t => (a -> Bool -> Bool -> b) -> t a -> t b
 withFirstLast f = mapWith $ f $-> limIt <-* limIt
@@ -84,13 +95,11 @@ withPrevNext f = mapWith $ f $-> adjElt <-* adjElt
 andPrevNext :: Traversable t => t a -> t (a, Maybe a, Maybe a)
 andPrevNext = withPrevNext (,,)
 
-
 {-
 There are two optimisations I'd like to make:
 1. in the case that we only traverse in one direction, we don't call both itMapL and itMapR
 2. in the case that we don't use the Traversable's elements (e.g. in adjElt), we don't construct the (init value, f...) pairs
 
 BUT: maybe after the optimising compilers been at it, neither of these are a problem?
+AND: I've not found good ways to do either.
 -}
-
-
