11ec966e51c974c6561be96c132ac519a8f18419
davjam <45334176+davjam@users.noreply.github.com>
2020-07-31 15:45:56 +0100

    INLINE -> INLINABLE
    performance still fine

--- src/CurryN.hs
+++ src/CurryN.hs
@@ -122,10 +122,10 @@ instead of
 
 Although these are only provided here for 1 to 4 arguments, you can use the "stacked tuple" to apply any number of arguments.
 -}
-{-# INLINE app1 #-}
-{-# INLINE app2 #-}
-{-# INLINE app3 #-}
-{-# INLINE app4 #-}
+{-# INLINABLE app1 #-}
+{-# INLINABLE app2 #-}
+{-# INLINABLE app3 #-}
+{-# INLINABLE app4 #-}
 
 type App1       a =             (a, ())
 -- ^ A "stacked tuple" of one value
